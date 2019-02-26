e78c69e70e993a33c83939b40593a2af265fc86c
Garik71426 <garo1997@mail.ru>
2019-02-26 19:21:47 +0400

    26.02

---
+++ 26.02.2019/Project/src/stores/UIStore.js
@@ -0,0 +1,12 @@
+import {extendObservable, action} from 'mobx';
+import Data from './../data/data.js'
+import {tableArr} from './../config/categoryConfig.js'
+
+
+
+class UIStore {
+    
+}
+
+
+export default UIStore ;
