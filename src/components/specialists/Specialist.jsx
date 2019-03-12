83371e72c60bae5f06150dd47ba9bdd5b0b3c773
AnahitKaryan <1anahit1@mail.ru>
2019-03-12 18:06:44 +0400

    18:06

--- src/components/specialists/Specialist.jsx
+++ src/components/specialists/Specialist.jsx
@@ -3,8 +3,11 @@ import {Button,Container, Row, Col, FormGroup, Label, Input } from 'reactstrap';
 import PropTypes from 'prop-types';
 import {observer} from 'mobx-react';
 
-import Messages from './../../Messages';
+import DateHourse from './form/DateHourse'
 import ChangeSpecialistInfo from './modals/ChangeSpecialistInfo'
+
+import Messages from './../../Messages';
+
 import './../../../assets/stylesheets/specialist.css';
 
 import NotFound from './../../NotFound'; 
@@ -72,51 +75,7 @@ class SpecialistUser extends Component {
 
                             </div>
                          </Col>
-                         {isUser === 'user' && <Col sm="4" >
-                             <h3 className = "textBlue"> {Messages.registered}</h3>
-                             <div>
-                                 <FormGroup tag="fieldset">
-                                     <legend>{Messages.specialist.chooseWork}</legend>
-                                     <FormGroup check>
-                                         <Label check>
-                                             <Input type="checkbox" />Կտրվածք (sa el petq e dataic lini)
-                                         </Label>
-                                     </FormGroup >
-                                     <FormGroup check>
-                                         <Label check>
-                                             <Input type="checkbox" />Սանրվածք (sa el petq e dataic lini)
-                                         </Label>
-                                     </FormGroup >
-                                     <FormGroup check>
-                                         <Label check>
-                                             <Input type="checkbox" />Ներկում (sa el petq e dataic lini)
-                                         </Label>
-                                     </FormGroup>
-                                 </FormGroup>
-                             </div>
-                             <div>
-                                 <FormGroup tag="fieldset">
-                                     <legend>{Messages.specialist.chooseClock}</legend>
-                                     <FormGroup check>
-                                         <Label check>
-                                             <Input type="radio" name="radio1" />09։00 (sa el chigitem vonc e linelu)
-                                         </Label>
-                                     </FormGroup>
-                                     <FormGroup check>
-                                         <Label check>
-                                             <Input type="radio" name="radio1" />12։ 30  (sa el chigitem vonc e linelu)
-                                         </Label>
-                                     </FormGroup>
-                                     <FormGroup check disabled>
-                                         <Label check>
-                                             <Input type="radio" name="radio1"  />15։15  (sa el chigitem vonc e linelu)
-                                         </Label>
-                                     </FormGroup>
-                                 </FormGroup>
-                             </div>
-                             <Button type = "submit" color="info" > {Messages.specialist.confirmed} </Button>
-
-                         </Col>}
+                         {isUser === 'user' && <DateHourse />}
                      </Row>
                      <h1 className = "textBlue" >{Messages.specialist.myWorkes}</h1>
                      <Row className = "mt-5">
